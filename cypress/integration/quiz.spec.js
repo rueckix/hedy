164d5533e0ae16623e8f34c1b20ce14602a11217
Lotus ter Haar <lotus.ter.haar@gmail.com>
2022-02-10 12:42:57 +0100

    Adjust the test code to the YAML file object. Repair some errors in the code (mostly wrongly used classname tags).

--- cypress/integration/quiz.spec.js
+++ cypress/integration/quiz.spec.js
@@ -1,4 +1,4 @@
-const allQuizData = require('../fixtures/quiz_data.json')
+const YAML = require('yamljs')
 
 function convertIndexToChar(index) {
     return String.fromCharCode(index + 65)
@@ -8,47 +8,51 @@ function convertCharToIndex(char) {
     return (char.charCodeAt(0) - 65);
 }
 
-// sanity check
-expect(allQuizData, 'list of quiz questions').to.be.an('array')
+describe('YAML file object', () => {
+    beforeEach('Read YAML file content into object', () => {
 
-describe('Quiz navigation', () => {
-    beforeEach('Navigate to quiz iframe', () => {
-        cy.fixture('quiz_data.json').as('quiz_data')
 
+        cy.readFile('./cypress/fixtures/quiz_en.yaml').then((str) => {
+            // parse the string into object literal
+            cy.wrap(YAML.parse(str)).as('quiz_data') // use a alias to make the quiz data available for all
+
+        })
     })
 
 
     it('Iterates over the questions', () => {
-        cy.visit('http://127.0.0.1:8080/hedy', {
-            onBeforeLoad(win) {
-                Object.defineProperty(win.navigator, 'language', {
-                    value: 'en-US'
-                })
-            }
-        }).its('navigator.language') // yields window.navigator.language
-            .should('equal', 'en-US') // asserts the expected value
+        cy.get('@quiz_data').then(quiz => {
+            cy.visit('http://127.0.0.1:8080/hedy', {
+                onBeforeLoad(win) {
+                    Object.defineProperty(win.navigator, 'language', {
+                        value: 'en-US'
+                    })
+                }
+            }).its('navigator.language') // yields window.navigator.language
+                .should('equal', 'en-US') // asserts the expected value
 
-        cy.get('div[data-tab="end"]').click()
-        cy.get('input#goto-quiz-btn').contains('Go to quiz').click()
-        cy.switchToIframe('#quiz-iframe').find('button.start-quiz-btn').contains('Go to question 1').click()
+            cy.get('div[data-tab="end"]').click()
+            cy.get('input#goto-quiz-btn').contains('Go to quiz').click()
+            cy.switchToIframe('#quiz-iframe').find('button.start-quiz-btn').contains('Go to question 1').click()
 
-        cy.screenshot()
-        cy.get('@quiz_data').then((quiz_data) => {
-            let q_nr = 1
+            cy.screenshot()
+
             let total_score = 0;
-            for (const question of quiz_data) {
+            let quiz_data = quiz[1]
+            for (let q_nr = 1; q_nr < Object.keys(quiz_data).length + 1; q_nr++) {
+                let question = quiz_data[q_nr]
                 let attempt_nr = 0;
-                const attempt_array = question['attempt'].split(',')
-                for (const untrimmed_attempt of attempt_array) {
-                    let attempt = untrimmed_attempt.trim() // the attempts characters can have whitespaces
+                cy.log(question)
+
+                for (const attempt of question['attempt']) {
                     let attempt_index = convertCharToIndex(attempt)
-                    expect(attempt_array).to.be.an('array').to.have.length.of.at.most(6) // the number of options can't be bigger than 6
+                    expect(question['attempt']).to.be.an('array').to.have.length.of.at.most(6) // the number of options can't be bigger than 6
                     cy.wait(5)
                     cy.switchToIframe('#quiz-iframe').find('.question-text').contains(question['question_text'])
 
                     const hasKeyCode = 'code' in question
                     if (hasKeyCode && (question['code'] != '...'))
-                        cy.switchToIframe('#quiz-iframe').find('.question-code').contains(`${question['code'].replace('\n', '<br>')}`)
+                        cy.switchToIframe('#quiz-iframe').find('.question-code').contains(`${question['code']}`)
 
                     const hasKeyOptionText = 'option_text' in question.mp_choice_options[attempt_index]
                     const hasKeyCodeText = 'code' in question.mp_choice_options[attempt_index]
@@ -69,36 +73,34 @@ describe('Quiz navigation', () => {
 
                 cy.switchToIframe('#quiz-iframe').find('.question-nr-length').contains(`Question ${q_nr} / 10`)
                 cy.switchToIframe('#quiz-iframe').find('.question-text-feedback').contains(question['question_text'])
-                if (attempt_array[attempt_array.length - 1] === question['correct_answer']) {
+                if (question['attempt'][question['attempt'].length - 1] === question['correct_answer']) {
                     cy.switchToIframe('#quiz-iframe').find('.feedback-circle-success').contains("Good!")
                     total_score += 10;
                 } else {
                     cy.switchToIframe('#quiz-iframe').find('.feedback-circle-failure').contains("Wrong!")
-
                 }
 
-                cy.log(question['mp_choice_options'][convertCharToIndex(attempt_array[attempt_array.length - 1].trim())]['feedback'])
-                //cy.switchToIframe('#quiz-iframe').find('.feedback-text').contains()
+                cy.log(question['mp_choice_options'][convertCharToIndex(question['attempt'][question['attempt'].length - 1])]['feedback'])
 
                 const hasKeyTextAsOption = 'option_text' in question['mp_choice_options'][convertCharToIndex(question['correct_answer'])]
-                if(hasKeyTextAsOption)
-                    cy.switchToIframe('#quiz-iframe').find('.correct-answer-text').contains(`The correct answer is: ${question['mp_choice_options'][convertCharToIndex(question['correct_answer'])]['option_text']}`)
+                if (hasKeyTextAsOption)
+                    cy.switchToIframe('#quiz-iframe').find('.correct-answer-text').contains(`${question['mp_choice_options'][convertCharToIndex(question['correct_answer'])]['option_text']}`)
                 const hasKeyCodeAsOption = 'code' in question['mp_choice_options'][convertCharToIndex(question['correct_answer'])]
-                if(hasKeyCodeAsOption)
-                    cy.switchToIframe('#quiz-iframe').find('.correct-answer-text').contains(`The correct answer is: ${question['mp_choice_options'][convertCharToIndex(question['correct_answer'])]['code']}`)
-                cy.switchToIframe('#quiz-iframe').find('.goto-question-btn').contains(`Go to question ${q_nr + 1}`).click()
-                cy.wait(10)
-                cy.screenshot()
+                if (hasKeyCodeAsOption)
+                    cy.switchToIframe('#quiz-iframe').find('.correct-answer-text').contains(`${question['mp_choice_options'][convertCharToIndex(question['correct_answer'])]['code']}`)
 
-                if (q_nr == quiz_data.length) // go to feedback page when the quiz hasn't ended yet
+                if (q_nr === Object.keys(quiz_data).length) // go to result page for the quiz page else go feedback page when the quiz hasn't ended yet
                 {
-                    cy.switchToIframe('#quiz-iframe').find('.end-quiz-text').contains('Quiz end')
+                    cy.switchToIframe('#quiz-iframe').find('.goto-question-btn').contains(`Go to quiz result`).click()
+                    cy.wait(10)
+                    cy.screenshot()
+                    cy.switchToIframe('#quiz-iframe').find('h1.end-quiz-text').contains('Quiz end')
                     cy.switchToIframe('#quiz-iframe').find('.score-text').contains('Score')
-                    cy.switchToIframe('#quiz-iframe').find('.total-score-text').contains('Score')
                     cy.switchToIframe('#quiz-iframe').find('.total-score-number').contains(total_score)
+                } else {
+                    cy.switchToIframe('#quiz-iframe').find('.goto-question-btn').contains(`Go to question ${q_nr + 1}`).click()
+                    cy.wait(10)
                 }
-                q_nr++;
-
             }
         })
     })
